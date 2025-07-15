4b0ae8ad97997c8cb7b19eaed9c89352a399229b
Le Nhat <nhatle.stu@gmail.com>
2025-07-15 11:50:56 -0600

    Examples with HTML templates

--- django/project_1/hello/views.py
+++ django/project_1/hello/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello!")
+    return render(request, "hello/index.html")
 
 def nolan(request):
     return HttpResponse("Hello, Nolan!")
@@ -12,4 +12,6 @@ def person(request):
     return HttpResponse("Hello there, have a nice day!")
 
 def greet(request, name):
-    return HttpResponse(f"Hello, {name.capitalize()}!")
+    return render(request, "hello/greet.html", {
+        "name": name.capitalize()
+    })
