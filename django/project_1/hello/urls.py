45bee232147b45903c50a7d4b78979880415abb7
Nhat Le <ift201@usask.ca>
2025-08-08 20:01:20 -0600

    Improved responsiveness with media query

--- django/project_1/hello/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("<str:name>", views.greet, name="greet"),
-    path("nolan", views.nolan, name="nolan"),
-    path("person", views.person, name="person")
-]
